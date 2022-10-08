6165f9cd0fda4931ad341bc817c2ea91b7cdd4bd
abhishekdana1999 <abhidana592@outlook.com>
2022-10-08 15:39:18 +0000

    change for script

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript" src="/script.js"></script>
+
       </Head>
 
       <main>
