6847c91a7c3437f9363701947ab46da3bd32c150
abhishekdana1999 <abhidana592@outlook.com>
2022-10-26 07:28:42 +0000

    Change

--- public/static/script.js
+++ public/static/script.js
@@ -115,7 +115,7 @@ function androidOrIOS() {
     return 'browser';
 }
 
-launchUri(androidOrIOS() == "android" ? 'app://com.atomyes' + window.location.pathname : 'app://com.atomyes' + window.location.pathname, function () {
+launchUri(androidOrIOS() == "android" ? 'app://com.atomyes' + window.location.pathname : 'app://com.yesbank' + window.location.pathname, function () {
     // SUCCESS - the protocol is registered and the user was asked to open
     // the URI in the appropriate application
     console.log("Great.");
