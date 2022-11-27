588c9ef03616b59080ec7c5765cc051d1d287512
abhishekdana1999 <abhidana592@outlook.com>
2022-11-27 05:46:23 +0000

    AASA

--- pages/404.js
+++ pages/404.js
@@ -4,9 +4,7 @@ import Link from 'next/link'
 
 export default function FourOhFour() {
   return <>
-  <Head>
-    <script type="text/javascript" src="/static/script.js"></script>
-  </Head>
+  
     <h1>404 - Custom Page Not Found</h1>
     <Link href="/">
       <a>
