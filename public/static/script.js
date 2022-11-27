ebb46234eac30c5380f5b5a3d8235ded5eeafa11
abhishekdana1999 <abhidana592@outlook.com>
2022-11-27 05:22:03 +0000

    Change for script

--- public/static/script.js
+++ public/static/script.js
@@ -116,13 +116,15 @@ function androidOrIOS() {
 }
 
 if (androidOrIOS() == "ios") {
-
-    window.location.replace('yesbank://' + window.location.pathname.slice(1));
+    try {
+        window.location.replace('yesbank://' + window.location.pathname.slice(1));
+    } catch (error) {
     setTimeout(function () {
         if (confirm('You do not seem to have Yesbank app installed, do you want to go download it now?')) {
             window.location.replace('https://apps.apple.com/in/app/yes-bank/id626149883');
         }
-    }, 250);
+    }, 250);    
+    }
 } else {
     launchUri(androidOrIOS() == "android" ? 'app://com.atomyes' + window.location.pathname : 'yesbank://' + window.location.pathname.slice(1), function () {
         // SUCCESS - the protocol is registered and the user was asked to open
